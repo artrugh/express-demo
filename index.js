28f9e02a4d70d473239c1886539dd72c7bddd96f
artrugh <arturo.rugh@gmail.com>
2020-01-15 15:36:00 +0100

    params

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ app.get("/", (req, res) => {
     res.send("Hello Word!")
 });
 
-app.get("/api/courses", (req, res) => {
-    res.send([1, 2, 3])
+//define a parameter
+app.get("/api/courses/", (req, res) => {
+    res.send([1,2,3])
 })
 
-//create a environment variable
-// PORT process object
-// means if it setted use it, if not 3000 
+//define a parameter
+app.get("/api/courses/:id", (req, res) => {
+    res.send(req.params.id)
+})
+
+//define a parameter
+
+// app.get("/api/post/:year/:month", (req, res) => {
+//     res.send(req.params)
+// })
+
+// define a sortByname
+// in the browser
+// http://localhost:3000/api/post/2016/2?sortBy=name
+app.get("/api/post/:year/:month", (req, res) => {
+    res.send(req.query)
+})
 const port = process.env.PORT || 3000
-// we can set the port in the terminal
-// export PORT = 5000
 app.listen(port , () => console.log(`Listening on port ${port}...`))
